44539ca4736105f354648d3773f85ed2c84c28a0
Nadia Putri <nadclputri@gmail.com>
2017-12-14 12:47:16 +0800

    Update function so the page displays the appropriate icon when a move is made

--- game.js
+++ game.js
@@ -11,46 +11,79 @@ function singleRound(user, computer) {
     let winner= "none";
     
     if (user == "rock") {
+        let playerUser = document.getElementById("player-user");
+        let playerComputer = document.getElementById("player-computer");
+        
+        playerUser.classList.replace("fa-hand-scissors", "fa-hand-rock");
+        playerUser.classList.replace("fa-hand-paper", "fa-hand-rock");
         switch (computer) {
             case "rock":
+                playerComputer.classList.replace("fa-hand-scissors", "fa-hand-rock");
+                playerComputer.classList.replace("fa-hand-paper", "fa-hand-rock");
                 alert("Draw! Both players used rock!")
                 winner = "none";
                 break;
             case "paper": 
+                playerComputer.classList.replace("fa-hand-scissors", "fa-hand-paper");
+                playerComputer.classList.replace("fa-hand-rock", "fa-hand-paper");
                 alert("You lose! Paper beats rock!")
                 winner = "computer";
                 break;
             case "scissor":
+                playerComputer.classList.replace("fa-hand-rock", "fa-hand-scissors");
+                playerComputer.classList.replace("fa-hand-paper", "fa-hand-scissors");
                 alert("You win! Rock beats scissor!")
                 winner = "user";
                 break;
         }
     } else if (user == "paper") {
+        let playerUser = document.getElementById("player-user");
+        let playerComputer = document.getElementById("player-computer");
+        
+        playerUser.classList.replace("fa-hand-scissors", "fa-hand-paper");
+        playerUser.classList.replace("fa-hand-rock", "fa-hand-paper");
         switch (computer) {
             case "paper":
+                playerComputer.classList.replace("fa-hand-scissors", "fa-hand-paper");
+                playerComputer.classList.replace("fa-hand-rock", "fa-hand-paper");
                 alert("Draw! Both players used paper!")
                 winner = "none";
                 break;
             case "scissor": 
+                playerComputer.classList.replace("fa-hand-rock", "fa-hand-scissors");
+                playerComputer.classList.replace("fa-hand-paper", "fa-hand-scissors");
                 alert("You lose! Scissor beats paper!")
                 winner = "computer";
                 break;
             case "rock":
+                playerComputer.classList.replace("fa-hand-scissors", "fa-hand-rock");
+                playerComputer.classList.replace("fa-hand-paper", "fa-hand-rock");
                 alert("You win! Paper beats rock!")
                 winner = "user";
                 break;
         }
     } else if (user == "scissor") {
+        let playerUser = document.getElementById("player-user");
+        let playerComputer = document.getElementById("player-computer");
+
+        playerUser.classList.replace("fa-hand-paper", "fa-hand-scissors");
+        playerUser.classList.replace("fa-hand-rock", "fa-hand-scissors");
         switch (computer) {
             case "scissor":
+                playerComputer.classList.replace("fa-hand-rock", "fa-hand-scissors");
+                playerComputer.classList.replace("fa-hand-paper", "fa-hand-scissors");
                 alert("Draw! Both players used scissor!")
                 winner = "none";
                 break;
             case "rock": 
+                playerComputer.classList.replace("fa-hand-scissors", "fa-hand-rock");
+                playerComputer.classList.replace("fa-hand-paper", "fa-hand-rock");
                 alert("You lose! Rock beats scissor!")
                 winner = "computer";
                 break;
             case "paper":
+                playerComputer.classList.replace("fa-hand-scissors", "fa-hand-paper");
+                playerComputer.classList.replace("fa-hand-rock", "fa-hand-paper");
                 alert("You win! Scissor beats paper!")
                 winner = "user";
                 break;
